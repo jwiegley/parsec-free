5cb7cffa0654aacbba710072b61b2cf5bca8fc24
John Wiegley <johnw@newartisans.com>
2016-10-11 09:58:12 -0700

    Improve both the semantics and output of logging

--- README.md
+++ README.md
@@ -6,47 +6,92 @@ None of your code should need to change.  Then, use `parseTestLog` rather than
 `parseTest`, and you should see output like the following:
 
 ```
-label "object name"
-  reserved "object"
-  identifier => "ActUp"
+label "object name" *
+  reserved "object" *
+  identifier => "aascllwl" *
 parserPlus
-  (reserved "identity")
-  return
-parserPlus
-  reserved "state"
-  label "variable declarations"
-    many1
-      label "type"
-        choice
-          reserved "int"
-      commaSep1
-        sepBy1
-          label "variable name"
-            identifier => "bar"
-          (string ",")
-      reservedOp ";"
-      (label "type")
+- (reserved "identity")
++ return
+parserPlus *
++ reserved "state" *
+  label "var decls" *
+    sepEndBy1 *
+    + label "var decl" *
+        label "type" *
+          choice *
+          - (reserved "int")
+          - (reserved "pointer")
+          + label "type name" *
+              identifier => "bool" *
+        commaSep1 *
+        + sepBy1 *
+          + label "variable name" *
+              identifier => "nlenshiy" *
+          - (string ",")
+    + symbol ";" *
+    - (label "var decl")
+        (label "type")
+          (choice)
+          - (reserved "int")
+          - (reserved "pointer")
+          - (label "type name")
+              (identifier)
+parserPlus *
++ reserved "events" *
+  sepEndBy1 *
+  + label "event defs" *
+      label "event definition kind" *
+        choice *
+        - (reserved "internal")
+        + reserved "imported" *
+      label "error keyword" *
+        parserPlus *
+        + reserved "error" *
+      commaSep1 *
+      + sepBy1 *
+        + label "event name" *
+            identifier => "swehegge" *
+          label "event params" *
+            parens *
+              between *
+              + string "(" *
+              + commaSep
+                + sepBy
+                  - (label "type")
+                      (choice)
+                      - (reserved "int")
+                      - (reserved "pointer")
+                      - (label "type name")
+                          (identifier)
+              + string ")" *
+          label "event definiton" *
+            parserPlus *
+            + reservedOp "=" *
+              label "expr" *
+                label "buildExpressionParser" *
+        - (string ",")
+  + symbol ";" *
+  - (label "event defs")
+      (label "event definition kind")
         (choice)
-          (reserved "int")
-          (reserved "pointer")
-          (label "user type name")
-            (identifier)
-parserPlus
-  reserved "events"
-  many1
-    label "event definition kind"
-      choice
-        (reserved "internal")
-        reserved "imported"
-    label "error keyword"
-      parserPlus
-        (reserved "error")
-        return
+        - (reserved "internal")
+        - (reserved "imported")
+        - (reserved "exported")
 ...
 ```
 
-In this output, terms in parentheses represent a failed attempt to parse along
-that branch. If a term ends with `...`, it means the parser did not complete
-before an exception occurred.
+Things to note in this output:
+
+- Terms in parentheses represent a failed attempt to parse along that branch.
+- If a term ends with `...`, it means the parser did not complete before an
+  exception occurred. This makes it easy to follow the chain of parsers "up to
+  the error", although there is no such output above.
+- Parsers with potentially more than 1 children use bullets to show those
+  children. These bullets have the following meaning: `-` if the parser
+  failed, `+` if it succeeded, `?` if it was pending completion before an
+  exception occurred.
+- A parser followed by `*` consumed input. Pay close attention to failed
+  parsers that consume input! They will prevent the second branch of `<|>`
+  from being attempted, or may indicate a need for `try`.
 
 Also, `parserPlus` is the name for `<|>`, while `label` is `<?>`.
--- Text/Parsec/Free/Log.hs
+++ Text/Parsec/Free/Log.hs
@@ -16,6 +16,7 @@ import                    Data.Foldable (forM_)
 import                    Data.IORef
 import                    Data.Map (Map)
 import qualified          Data.Map as M
+import qualified "parsec" Text.Parsec.Error as P
 import                    Text.Parsec.Free
 import                    Text.Parsec.Free.Eval
 import qualified "parsec" Text.Parsec.Prim as P
@@ -26,23 +27,26 @@ type LogParsecT s u m a = MonadReader LogType m => P.ParsecT s u m a
 
 data ParseLog
     = forall s u m a. ParseAttempt Bool (ParsecF s u m a)
-    | forall s u m a. ParseFailed (ParsecF s u m a)
-    | forall s u m a b. Show b => ParseSuccess b (ParsecF s u m a)
-    | forall s u m a. ParseSuccessful (ParsecF s u m a)
+    | forall s u m a. ParseFailed Bool P.ParseError (ParsecF s u m a)
+    | forall s u m a b. Show b => ParseSuccess Bool b (ParsecF s u m a)
+    | forall s u m a. ParseSuccessful Bool (ParsecF s u m a)
     | Indent Bool
     | Dedent
 
 instance Show ParseLog where
-    show (ParseAttempt b p)  = "ParseAttempt " ++ show b ++ " " ++ show p
-    show (ParseFailed p)     = "ParseFailed " ++ show p
-    show (ParseSuccess a p)  = "ParseSuccess " ++ show a ++ " " ++ show p
-    show (ParseSuccessful p) = "ParseSuccessful " ++ show p
-    show (Indent b)          = "Indent " ++ show b
-    show Dedent              = "Dedent"
+    show (ParseAttempt b p)    = "ParseAttempt " ++ show b ++ " " ++ show p
+    show (ParseFailed b err p) = "ParseFailed " ++ show b ++ " " ++ show err ++ " " ++ show p
+    show (ParseSuccess b a p)  = "ParseSuccess " ++ show b ++ " " ++ show a ++ " " ++ show p
+    show (ParseSuccessful b p) = "ParseSuccessful " ++ show b ++ " " ++ show p
+    show (Indent b)            = "Indent " ++ show b
+    show Dedent                = "Dedent"
 
 $(makePrisms ''ParseLog)
 
-data Result = Failure | Success | SuccessValue String | Pending
+data Result = Failure Bool P.ParseError
+            | Success Bool
+            | SuccessValue Bool String
+            | Pending
 
 data LogEntry = LogEntry
     { _leDepth  :: Int
@@ -59,16 +63,23 @@ instance Show LogEntry where
         (if _leBranch
          then replicate (pred _leDepth * 2) ' '
                 ++ case _leResult of
-                    Failure        -> "- "
-                    Success        -> "+ "
-                    SuccessValue _ -> "+ "
-                    Pending        -> "? "
+                    Failure _ _err   -> "- "
+                    Success _        -> "+ "
+                    SuccessValue _ _ -> "+ "
+                    Pending          -> "? "
          else replicate (_leDepth * 2) ' ')
-          ++ case _leResult of
-              Failure          -> "("  ++ _leParser ++ ")"
-              Success          -> _leParser
-              SuccessValue str -> _leParser ++ " => " ++ str
-              Pending          -> _leParser ++ "..."
+            ++ (case _leResult of
+                    Failure _ _err     -> "("  ++ _leParser ++ ")"
+                    Success _          -> _leParser
+                    SuccessValue _ str -> _leParser ++ " => " ++ str
+                    Pending          -> _leParser ++ "...")
+            ++ if case _leResult of
+                      Failure b _      -> b
+                      Success b        -> b
+                      SuccessValue b _ -> b
+                      Pending          -> False
+               then " *"
+               else ""
 
 data RenderState = RenderState
     { _rsIndex  :: Int
@@ -109,46 +120,63 @@ renderLog showAll l =
             rsIndex += 1
             go n xs
 
-        ParseFailed _     -> setResult Failure >> go n xs
-        ParseSuccessful _ -> setResult Success >> go n xs
-        ParseSuccess v _  -> setResult (SuccessValue (show v)) >> go n xs
+        ParseFailed b err _ -> setResult (Failure b err) >> go n xs
+        ParseSuccessful b _ -> setResult (Success b) >> go n xs
+        ParseSuccess b v _  -> setResult (SuccessValue b (show v)) >> go n xs
       where
         setResult str = do
             i <- gets (^?! rsStack._head)
             rsMap.ix i.leResult .= str
             rsStack %= tail
 
-appendLog :: MonadIO m => ParseLog -> LogParsecT s u m ()
-appendLog l = do
-    ref <- lift ask
-    liftIO $ modifyIORef ref (l:)
+appendLog :: (MonadIO m, MonadReader LogType m) => ParseLog -> m ()
+appendLog l = ask >>= \ref -> liftIO (modifyIORef ref (l:))
 
 attempt :: MonadIO m
         => Bool -> ParsecF s u' m b -> LogParsecT s u m a
         -> LogParsecT s u m a
-attempt b t p = do
+attempt b t p = P.mkPT $ \s -> do
     appendLog (ParseAttempt b t)
-    P.parserPlus
-        (P.try p <* appendLog (ParseSuccessful t))
-        (appendLog (ParseFailed t) >> P.parserZero)
+    res <- P.runParsecT p s
+    r   <- parserReply res
+    let consumed = case res of
+            P.Consumed _ -> True
+            P.Empty    _ -> False
+    case r of
+        P.Ok _ _ _  -> appendLog (ParseSuccessful consumed t)
+        P.Error err -> appendLog (ParseFailed consumed err t)
+    return res
+  where
+    parserReply res = case res of
+        P.Consumed r -> r
+        P.Empty    r -> r
 
 attemptShow :: (MonadIO m, Show a)
             => Bool -> ParsecF s u' m b -> LogParsecT s u m a
             -> LogParsecT s u m a
-attemptShow b t p = do
+attemptShow b t p = P.mkPT $ \s -> do
     appendLog (ParseAttempt b t)
-    P.parserPlus
-        (do a <- P.try p
-            appendLog (ParseSuccess a t)
-            return a)
-        (appendLog (ParseFailed t) >> P.parserZero)
+    res <- P.runParsecT p s
+    r   <- parserReply res
+    let consumed = case res of
+            P.Consumed _ -> True
+            P.Empty    _ -> False
+    case r of
+        P.Ok x _ _  -> appendLog (ParseSuccess consumed x t)
+        P.Error err -> appendLog (ParseFailed consumed err t)
+    return res
+  where
+    parserReply res = case res of
+        P.Consumed r -> r
+        P.Empty    r -> r
 
-indented :: MonadIO m => Bool -> LogParsecT s u m a -> LogParsecT s u m a
-indented b p = do
+indented :: MonadIO m
+         => Bool -> LogParsecT s u m a -> LogParsecT s u m a
+indented b p = P.mkPT $ \s -> do
     appendLog (Indent b)
-    P.parserPlus
-        (P.try p <* appendLog Dedent)
-        (appendLog Dedent >> P.parserZero)
+    res <- P.runParsecT p s
+    appendLog Dedent
+    return res
 
 evalLog :: (MonadIO m, P.Stream s m t)
         => ParsecDSL s u m a -> LogParsecT s u m a
@@ -164,10 +192,10 @@ dumpLog theLog = flip evalStateT (0, M.empty :: Map Int String) $
                 liftIO $ putStrLn $ p ++ " /= " ++ p'
             indent (i-1) >> put (i-1, M.delete i m)
     case l of
-        ParseAttempt _ p  -> indent i >> put (i+1, M.insert i (show p) m)
-        ParseSuccess _ p  -> go (show p)
-        ParseSuccessful p -> go (show p)
-        ParseFailed p     -> go (show p)
+        ParseAttempt _ p    -> indent i >> put (i+1, M.insert i (show p) m)
+        ParseSuccess _ _ p  -> go (show p)
+        ParseSuccessful _ p -> go (show p)
+        ParseFailed _ _ p   -> go (show p)
         _ -> return ()
     case l of
         Indent _ -> return ()
